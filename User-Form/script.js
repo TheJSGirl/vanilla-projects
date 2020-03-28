863be48a8e3d39b0f7e181be05fa0edaf8495d82
TheJSGirl <prinegi100@gmail.com>
2020-03-28 17:05:59 +0530

    refactor: removed redudant code

--- User-Form/script.js
+++ User-Form/script.js
@@ -22,37 +22,18 @@ const isValidEmail = (email) => {
   const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return res.test(String(email).toLowerCase());
 }
+
+const checkRequiredFields = (inputArray) => {
+  inputArray.forEach((input) => {
+    if(input.value.trim() === '') {
+      showError(input, `${input.id} is required`)
+    } else {
+      showSuccess(input)
+    }
+  })
+}
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  //check if username value is empty
-  if(username.value === '') {
-    showError(username, 'Username is required');
-  }
-  else {
-    showSuccess(username);
-  }
-
-  if(email.value === '') {
-    showError(email, 'Email is required');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  }
-  else {
-    showSuccess(email);
-  }
-
-  if(password.value === '') {
-    showError(password, 'Password is required');
-  }
-  else {
-    showSuccess(password);
-  }
-
-  if(password2.value === '') {
-    showError(password2, 'Confirm Password is required');
-  }
-  else {
-    showSuccess(password2);
-  }
+  checkRequiredFields([username, email, password, password2]);
 })
